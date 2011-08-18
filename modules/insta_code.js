fad36be87aab66135cd8ef7591b3aa12a770e771
Ibrahim Al-Rajhi <abrahamalrajhi@gmail.com>
2011-08-18 16:10:01 -0400

    added new module to be worked on

---
+++ modules/insta_code.js
@@ -0,0 +1,25 @@
+;(function(modules) {
+
+    modules['instaCode'] = {
+          'author': 'Ibrahim Al-Rajhi'
+        , 'name': 'Insta Code'
+        , 'description': 'Inject your own scripts into Grooveshark.'
+        , 'isEnabled': true
+        , 'style': false
+        , 'setup': setup
+        , 'construct': construct
+        , 'destruct': destruct
+        , 'options': false
+    };
+    
+    function setup() {
+    }
+
+    function construct() {
+    }
+
+    function destruct() {
+    }
+
+})(ges.modules.modules);
+
